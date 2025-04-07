122381519b3b136a00a25bf70a9b0865b8159c49
ChiefLordNord <159925266+ChiefLordNord@users.noreply.github.com>
2025-04-07 01:09:40 +0000

    Commit to AntonioC

--- map.js
+++ map.js
@@ -128,4 +128,52 @@ let audioSelectionMenu = false;
             }
         }
         }
-    
+        let movingObject = { x: 0, y: 0, speed: 2, path: [] };
+    let pathIndex = 0;
+
+    function createPath() {
+        movingObject.path = [
+            { x: 100, y: 100 },
+            { x: 300, y: 100 },
+            { x: 300, y: 300 },
+            { x: 100, y: 300 },
+            { x: 100, y: 100 }
+        ];
+        movingObject.x = movingObject.path[0].x;
+        movingObject.y = movingObject.path[0].y;
+    }
+
+    function moveAlongPath() {
+        if (pathIndex < movingObject.path.length - 1) {
+            let target = movingObject.path[pathIndex + 1];
+            let dx = target.x - movingObject.x;
+            let dy = target.y - movingObject.y;
+            let distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < movingObject.speed) {
+                movingObject.x = target.x;
+                movingObject.y = target.y;
+                pathIndex++;
+            } else {
+                movingObject.x += (dx / distance) * movingObject.speed;
+                movingObject.y += (dy / distance) * movingObject.speed;
+            }
+        }
+    }
+
+    function drawMovingObject() {
+        fill(255, 0, 0);
+        ellipse(movingObject.x, movingObject.y, 20, 20);
+    }
+
+    createPath();
+
+    function draw() {
+        background(0);
+        image(map, 0, 0, windowWidth, windowHeight);
+        for (let btn of buttons) {
+            btn.display();
+        }
+        moveAlongPath();
+        drawMovingObject();
+    }
